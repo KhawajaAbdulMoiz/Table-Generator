3ae11bf8e6268df3f6f9a42abd17b6e4dce627b8
Khawaja Abdul Moiz <156238498+KhawajaAbdulMoiz@users.noreply.github.com>
2024-04-08 23:50:23 +0500

    Add files via upload

---
+++ TableGenerator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int num;
+	
+	
+	cout<<"Enter Number  : ";
+	cin>>num;
+	
+	for(int i=0;i<=num;++i){
+		cout<<num<<"*"<<i<<"="<<num*i<<endl;
+		
+	}
+	
+	
+	}
+
+		
+
+
+
+
